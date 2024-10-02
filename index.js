b70fa4dc04d2f0d146015abc3ea5c476f053b2cd
dashubei <dashubei6@gmail.com>
2024-10-02 22:35:37 +0900

    20241002 型練習

---
+++ index.js
@@ -0,0 +1,20 @@
+// 型の練習をします
+//「プリミティブ型」
+// string型
+var myName = "Koki";
+console.log(myName);
+// number型
+var myNum = 123;
+console.log(myNum);
+// bigint型 (基本はnumber型で事足りるようだ)
+var bigNum = 10n;
+console.log(bigNum);
+// boolean型
+var bool = true;
+console.log(bool);
+// null型
+var nullKata = null;
+console.log(nullKata);
+// undefined型
+var unde = undefined;
+console.log(unde);
---
+++ index.ts
@@ -0,0 +1,25 @@
+// 型の練習をします
+//「プリミティブ型」
+// string型
+const myName: string = "Koki";
+console.log(myName);
+
+// number型
+const myNum: number = 123;
+console.log(myNum);
+
+// bigint型 (基本はnumber型で事足りるようだ)
+const bigNum: bigint = 10n;
+console.log(bigNum);
+
+// boolean型
+const bool: boolean = true;
+console.log(bool);
+
+// null型
+const nullKata: null = null;
+console.log(nullKata);
+
+// undefined型
+const unde: undefined = undefined;
+console.log(unde);
